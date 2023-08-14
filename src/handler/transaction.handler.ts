ad3a8f6d42cfec45e16b01389bb9836a1d6cf0c8
Michelle Fialkoff <mfialkoff@Michelles-MBP.attlocal.net>
2023-08-14 11:36:06 -0500

    Updated transactions

--- src/handler/transaction.handler.ts
+++ src/handler/transaction.handler.ts
@@ -2,6 +2,8 @@ import { TRANSACTIONS } from "../data/transaction.data";
 import { CUSTOMERS, getCustomer } from "../data/customer.data";
 import { Customer } from "../interface/customer.interface";
 import { Transaction } from "../interface/transaction.interface";
+import { Message } from "../interface/message.interface";
+import { DISCOUNT } from "./discount.handler";
 
 interface TransactionInput {
     customerId: string;
@@ -9,28 +11,28 @@ interface TransactionInput {
     discountCode?: string;
 };
 
-export async function transaction(input: TransactionInput): Promise<any> {
-    // Get Customer and validate discount code
-    const customer: Customer = getCustomer(input.customerId);
-    if (input.discountCode && customer.discount && customer.discount.discountCode != input.discountCode) {
-        throw new Error(`Discount code is not valid.`);
-    };
+export async function transaction(input: TransactionInput): Promise<Message> {
+    // Validate and Use Discount:
+    let discountUsed = false
+    if (DISCOUNT.isActive && DISCOUNT.discountCode === input.discountCode) {
+        discountUsed = true;
+    }
 
-    // Add the transaction record to the database
+    // Make Transaction:
     const transaction: Transaction = {
         items: input.items,
-        discountUsed: input.discountCode ? true : false
+        discountUsed: discountUsed
     }
     TRANSACTIONS.push(transaction)
 
-    if (input.discountCode) {
-        // Remove discount code from customer record
-        CUSTOMERS.set(input.customerId, {
-            id: input.customerId,
-        })
+    // Update Discount:
+    if (DISCOUNT.nthTransaction && TRANSACTIONS.length % DISCOUNT.nthTransaction === 0) {
+        DISCOUNT.isActive = true;
     }
 
+    let message = `Transaction complete ${discountUsed ? `with a discount code.` : `with not discounts applied.`}` ;
+
     return {
-        message: `Transaction complete.`
+        message: message
     };
 }
