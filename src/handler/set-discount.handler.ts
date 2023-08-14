a1e3b32f481a1ea18649c49e7166a0acef52e1c7
Michelle Fialkoff <mfialkoff@Michelles-MBP.attlocal.net>
2023-08-14 11:13:27 -0500

    Updated set-discount logic

--- src/handler/set-discount.handler.ts
+++ src/handler/set-discount.handler.ts
@@ -1,22 +1,19 @@
-export interface NextDiscount {
-    numberOfLogins: number;
+export interface Discount {
+    nthTransaction: number;
     discountCode: string;
 }
 
-export let NEXT_DISCOUNT: NextDiscount;
+export let DISCOUNT: Discount;
 
 interface SetDiscountInput {
-    numberOfLogins: number;
+    nthTransaction: number;
     discountCode: string;
 };
 
 export async function setDiscount(input: SetDiscountInput): Promise<any> {
     console.log(`Setting discount with input values: ${JSON.stringify(input, null, 2)}`);
-    NEXT_DISCOUNT = {
-        numberOfLogins: input.numberOfLogins,
-        discountCode: input.discountCode
-    };
-    console.log(`Creating discount: ${JSON.stringify(NEXT_DISCOUNT, null, 2)}`);
+    DISCOUNT = input;
+    console.log(`Creating discount: ${JSON.stringify(DISCOUNT, null, 2)}`);
 
     return {
         message: 'Discount successfully set'
