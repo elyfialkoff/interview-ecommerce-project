746c9e147db5b646716bc7b992f854b158307765
Michelle Fialkoff <mfialkoff@Michelles-MBP.attlocal.net>
2023-08-14 11:23:58 -0500

    Updated login

--- src/handler/login.ts
+++ src/handler/login.ts
@@ -1,31 +1,15 @@
-import { CUSTOMERS } from "../data/customer.data";
 import { Customer } from "../interface/customer.interface";
-import { DISCOUNT } from "./discount.handler";
-
-export let LOGIN_COUNT: number = 0;
+import { getDiscount } from "./discount.handler";
 
 interface GetCustomerInput {
     customerId: string;
 }
 
 export async function login(input: GetCustomerInput): Promise<Customer> {
-    LOGIN_COUNT += 1;
-    console.log(`Login Count: ${LOGIN_COUNT}`);
-
     const customer: Customer = {
-        id: input.customerId
+        id: input.customerId,
+        discount: await getDiscount()
     };
 
-    if (LOGIN_COUNT % DISCOUNT.nthTransaction === 0) {
-        console.log(`Issuing a discount to ${input.customerId}.`)
-        customer['discount'] = {
-            nthTransaction: 0,
-            isActive: true,
-            discountCode: DISCOUNT.discountCode
-        };
-    };
-    console.log(`Storing ${JSON.stringify(customer, null, 2)}.`)
-    CUSTOMERS.set(input.customerId, customer)
-    
-    return CUSTOMERS.get(input.customerId)!;
+    return customer;
 }
